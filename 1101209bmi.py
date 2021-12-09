1df1d9c07d0de143b724f21f75e58b9039e2dfc4
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-09 19:57:24 +0800

    Update 1101209bmi.py

--- 1101209bmi.py
+++ 1101209bmi.py
@@ -4,10 +4,10 @@ h = st.number_input(input('請輸入身高(M)？'))
 bmi = BMI(w, h)
 st.('BMI為', bmi)
 if (bmi < 18):
-    print('體重過輕')
+   st.write('體重過輕')
 elif (bmi < 24):
-    print('體重正常')
+    st.write('體重正常')
 elif (bmi < 27):
-    print('體重過重')
+    st.write('體重過重')
 else:
-    print('體重肥胖')
+    st.write('體重肥胖')
